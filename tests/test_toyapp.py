a6909006c541e398b68225d9fcf6d8a6144a2566
ian1roberts <ian.roberts@cantab.net>
2024-02-23 13:32:24 +0000

    Add test case

--- tests/test_toyapp.py
+++ tests/test_toyapp.py
@@ -4,10 +4,10 @@
 
 
 import unittest
+
 from click.testing import CliRunner
 
-from toyapp import toyapp
-from toyapp import cli
+from toyapp import cli, toyapp
 
 
 class TestToyapp(unittest.TestCase):
@@ -15,19 +15,24 @@ class TestToyapp(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.num1 = "5.7"
+        self.num2 = "3.2"
+        self.operator = "+"
+        self.answer = 8.9
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+    def test_001_addition_num1_num2_operator(self):
         """Test something."""
+        assert toyapp.math_func(self.num1, self.num2, self.operator) == self.answer
 
     def test_command_line_interface(self):
         """Test the CLI."""
         runner = CliRunner()
-        result = runner.invoke(cli.main)
+        result = runner.invoke(cli.main, [self.num1, self.num2, self.operator])
         assert result.exit_code == 0
-        assert 'toyapp.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
+        assert "The answer to" in result.output
+        help_result = runner.invoke(cli.main, ["--help"])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert "--help  Show this message and exit." in help_result.output
--- toyapp/cli.py
+++ toyapp/cli.py
@@ -1,15 +1,32 @@
 """Console script for toyapp."""
+
 import sys
+
 import click
+
+from toyapp.toyapp import math_func
 
 
 @click.command()
-def main(args=None):
-    """Console script for toyapp."""
-    click.echo("Replace this message by putting your code into "
-               "toyapp.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+@click.argument("num1", type=click.FLOAT)
+@click.argument("num2", type=click.FLOAT)
+@click.argument("operation", default="+", type=click.STRING)
+def main(num1: float, num2: float, operation: str):
+    """Perform simple arithmetic operations given two numbers and an operator.
+
+    If operator is ommited, the default is addition.
+    To prevent * being interpreted as a wildcard, use quotes around the operator.
+
+    num1: float - The first number
+    num2: float - The second number
+    operation: str - The operation to perform on the two numbers
+
+    Evaluates the expression num1 operation num2 and prints the result.
+    """
+    answer = math_func(num1, num2, operation)
+    print(f"\n\tThe answer to {num1} {operation} {num2} = {answer:.2f}\n")
+
+    return float(answer)
 
 
 if __name__ == "__main__":
